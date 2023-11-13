196abe9848da39390a06c8806cfafd4c92f343d3
Wandering Raven <igorfm13524@gmail.com>
2023-11-13 10:59:05 -0300

    Traduz contador para Portugês

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Contador Vite PWA</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+        Para saber mais, clique nos logos do Vite ou TypeScript
     </p>
   </div>
 `
