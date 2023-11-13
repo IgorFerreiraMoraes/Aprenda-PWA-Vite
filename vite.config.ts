1a0adfdb12c70ae670ae88cfc9cf9090218cc9cf
Wandering Raven <igorfm13524@gmail.com>
2023-11-13 19:45:38 -0300

    Adiciona caminho base para usar com GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/Aprenda-PWA-Vite/',
 	plugins: [
 		VitePWA({
 			registerType: 'autoUpdate',
