3402345d6adeae1d42c2a9f8b00529377a865070
Wandering Raven <igorfm13524@gmail.com>
2023-11-13 11:01:07 -0300

    Cria webmanifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,25 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['vite.svg'],
+			manifest: {
+				name: 'Contador Vite PWA',
+				short_name: 'Contador PWA',
+				description:
+					'Um contador feito com Vite para demonstrar como criar uma PWA e instlá-la em dispositivos móveis',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: '/vite.svg',
+						sizes: 'any',
+						type: 'image/svg+xml',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 });
